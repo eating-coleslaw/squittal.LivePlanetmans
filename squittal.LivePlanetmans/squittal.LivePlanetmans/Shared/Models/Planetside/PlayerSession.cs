f0c560c711d9496dd14eb2415fd85624822440e5
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2019-10-03 09:06:10 -0500

    Add PlayerSession class

---
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/Planetside/PlayerSession.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace squittal.LivePlanetmans.Shared.Models
+{
+    public class PlayerSession
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CharacterId { get; set; }
+
+        public DateTime LoginDate { get; set; }
+        public DateTime LogoutDate { get; set; }
+
+        public int Duration { get; set; } // Total Milliseconds
+
+        public int? Kills { get; set; }
+        public int? Deaths { get; set; }
+        public int? Headshots { get; set; }
+        public int? Teamkills { get; set; }
+        public int? Suicides { get; set; }
+    }
+}
