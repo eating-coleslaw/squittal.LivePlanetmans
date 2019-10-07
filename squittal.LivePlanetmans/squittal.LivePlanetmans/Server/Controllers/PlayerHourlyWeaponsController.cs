99cd4f6a48dddf09a2a4807caf162595dbb79dbb
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2019-10-07 15:37:02 -0500

    add working top weapons by kills query

---
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Server/Controllers/PlayerHourlyWeaponsController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using squittal.LivePlanetmans.Server.Data;
+using squittal.LivePlanetmans.Server.Services.Planetside;
+using squittal.LivePlanetmans.Shared.Models;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace squittal.LivePlanetmans.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayerHourlyWeaponsController : ControllerBase
+    {
+
+        private readonly IDbContextHelper _dbContextHelper;
+
+        public PlayerHourlyWeaponsController (IDbContextHelper dbContextHelper)
+        {
+            _dbContextHelper = dbContextHelper;
+        }
+
+        [HttpGet("weaponKills/{characterId}")]
+        public async Task<ActionResult<IEnumerable<HourlyWeaponSummaryRow>>> GetPlayerTopWeaponsByKillsAsync(string characterId)
+        {
+            DateTime nowUtc = DateTime.UtcNow;
+            DateTime startTime = nowUtc - TimeSpan.FromHours(1);
+
+            using (var factory = _dbContextHelper.GetFactory())
+            {
+                var dbContext = factory.GetDbContext();
+
+                IQueryable<HourlyWeaponSummaryRow> query =
+                    from death in dbContext.Deaths
+
+                    where death.Timestamp >= startTime
+                       && death.AttackerCharacterId == characterId
+                       && death.DeathEventType == DeathEventType.Kill
+                    
+                    //group death by (int)death.AttackerWeaponId into weaponsGroup
+
+                    join weapon in dbContext.Items
+                      on (int)death.AttackerWeaponId equals weapon.Id into weaponsQ
+                      //on(int)death.AttackerWeaponId equals weapon.Id into weaponsQ
+                    from weapon in weaponsQ.DefaultIfEmpty()
+                    
+
+                    //group death by death.AttackerWeaponId into weaponsGroup
+
+                    //from death in weaponsGroup //.DefaultIfEmpty()
+
+                    //join weapon in dbContext.Items
+                    //  //on new { GroupKey = (int?)weaponsGroup.Key.Value } equals new { WeaponId = (int?)weapon.Id } into weaponsQ
+                    //  on death.AttackerWeaponId equals weapon.Id
+                    //from weapon in weaponsQ //.DefaultIfEmpty()
+                    //on weaponsGroup.Key equals weapon.Id into weaponsQ
+
+                    //on death.AttackerWeaponId equals weapon.Id into weaponsQ
+
+                    select new HourlyWeaponSummaryRow()
+                    {
+                        WeaponId = (int)death.AttackerWeaponId, // ?? 0,
+                        WeaponName = weapon.Name, //"hello",
+                        FactionId = weapon.FactionId, //3,
+                        Kills = (from kill in dbContext.Deaths
+                                 where kill.AttackerCharacterId == characterId
+                                    && kill.DeathEventType == DeathEventType.Kill
+                                    && kill.AttackerWeaponId == weapon.Id
+                                    && kill.Timestamp >= startTime
+                                 //group kill by kill.AttackerWeaponId into weaponGroup
+                                 select kill.AttackerWeaponId).Count(), //GroupBy(k => k.AttackerWeaponId).Count(),
+                        ///Count(d => d.AttackerWeaponId == weaponsGroup.Key), //15,
+                        
+                        Headshots = (from kill in dbContext.Deaths
+                                     where kill.AttackerCharacterId == characterId
+                                        && kill.DeathEventType == DeathEventType.Kill
+                                        && kill.IsHeadshot == true
+                                        && kill.AttackerWeaponId == weapon.Id
+                                        && kill.Timestamp >= startTime
+                                     //group kill by kill.AttackerWeaponId into weaponGroup
+                                     select kill.AttackerWeaponId).Count(), //GroupBy(k => k.AttackerWeaponId).Count()
+
+                        //weaponsGroup.Count(d => d.AttackerWeaponId == weaponsGroup.Key && d.IsHeadshot == true) //7
+
+                        //WeaponId = (int)weaponsGroup.Key, //death.AttackerWeaponId, // Id, /*weaponsGroup.Key ?? 0,*/
+                        //WeaponName = weapon.Name, //Where(w => (int)weaponsGroup.Key == w.Id).Select(w => w.Name).FirstOrDefault(),//weaponsQ.FirstOrDefault().Name,
+                        //FactionId = weapon.FactionId, //weaponsQ.Where(w => (int)weaponsGroup.Key == w.Id).Select(w => w.FactionId).FirstOrDefault(),//weaponsQ.FirstOrDefault().FactionId,
+                        //Kills = weapon.Id,//weaponsGroup.Count(kill => kill.AttackerCharacterId == characterId
+                        ////&& kill.DeathEventType == DeathEventType.Kill
+                        ////&& (int)kill.AttackerWeaponId == (int)weaponsGroup.Key
+                        ////&& kill.Timestamp >= startTime),
+                        ////weaponsGroup.Where(kill => kill.AttackerCharacterId == characterId
+                        ////                                && kill.DeathEventType == DeathEventType.Kill
+                        ////                                && kill.AttackerWeaponId == weaponsGroup.Key
+                        ////                                && kill.Timestamp >= startTime).Count(),
+                        ////(from kill in dbContext.Deaths
+                        //// where kill.AttackerCharacterId == characterId
+                        ////    && kill.DeathEventType == DeathEventType.Kill
+                        ////    && (int)kill.AttackerWeaponId == (int)weaponsGroup.Key
+                        ////    && kill.Timestamp >= startTime
+                        //// select kill).Count(),
+                        //Headshots = weapon.Id / 2//weaponsGroup.Count(kill => kill.AttackerCharacterId == characterId
+                        //            //&& kill.DeathEventType == DeathEventType.Kill
+                        //            //&& (int)kill.AttackerWeaponId == (int)weaponsGroup.Key
+                        //            //&& kill.Timestamp >= startTime
+                        //            //&& kill.IsHeadshot == true)
+                        //            //weaponsGroup.Where(kill => kill.AttackerCharacterId == characterId
+                        //            //                                && kill.DeathEventType == DeathEventType.Kill
+                        //            //                                && kill.AttackerWeaponId == weaponsGroup.Key
+                        //            //                                && kill.Timestamp >= startTime
+                        //            //                                && kill.IsHeadshot == true).Count()
+                        ////(from kill in dbContext.Deaths
+                        //// where kill.AttackerCharacterId == characterId
+                        ////    && kill.DeathEventType == DeathEventType.Kill
+                        ////    && kill.IsHeadshot == true
+                        ////    && (int)kill.AttackerWeaponId == (int)weaponsGroup.Key
+                        ////    && kill.Timestamp >= startTime
+                        //// select kill).Count()
+                    };
+
+                var topWeapons = await query
+                                        .AsNoTracking()
+                                        .ToArrayAsync();
+
+                return topWeapons
+                        .GroupBy(w => w.WeaponId)
+                        .Select(grp => new HourlyWeaponSummaryRow()
+                        {
+                            WeaponId = grp.Key,
+                            WeaponName = grp.Where(w => w.WeaponId == grp.Key).Select(w => w.WeaponName).FirstOrDefault(),
+                            FactionId = grp.Where(w => w.WeaponId == grp.Key).Select(w => w.FactionId).FirstOrDefault(),
+                            Kills = grp.Where(w => w.WeaponId == grp.Key).Select(w => w.Kills).FirstOrDefault(),
+                            Headshots = grp.Where(w => w.WeaponId == grp.Key).Select(w => w.Headshots).FirstOrDefault()
+                        })
+                        .OrderByDescending(w => w.Kills)
+                        .Where(w => w.Kills > 0)
+                        .ToArray();
+            }
+        }
+    }
+}
---
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/squittal/PlayerHourlyWeaponsSummary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace squittal.LivePlanetmans.Shared.Models
+{
+    public class PlayerHourlyWeaponsSummary
+    {
+        public string PlayerId { get; set; }
+        public DateTime QueryStartTime { get; set; }
+        public DateTime QueryNowUtc { get; set; }
+
+        public HourlyWeaponSummaryRow[] TopWeaponsByKills { get; set; }
+        public HourlyWeaponSummaryRow[] TopWeaponsByDeaths { get; set; }
+
+        //public IEnumerable<HourlyWeaponSummaryRow> TopWeaponsByKills { get; set; }
+        //public IEnumerable<HourlyWeaponSummaryRow> TopWeaponsByDeaths { get; set; }
+
+    }
+
+    public class HourlyWeaponSummaryRow
+    {
+        public int WeaponId { get; set; }
+        public string WeaponName { get; set; }
+        public int? FactionId { get; set; }
+        
+        public int Kills { get; set; }
+        public int Headshots { get; set; }
+        public double HeadshotRatio
+        {
+            get
+            {
+                return GetHeadshotRatio(Kills, Headshots);
+            }
+        }
+        
+        //public int DeathsAgainst { get; set; }
+        //public int HeadshotsAgainst { get; set; }
+        //public double HeadshotRatioAgainst
+        //{
+        //    get
+        //    {
+        //        return GetHeadshotRatio(DeathsAgainst, HeadshotsAgainst);
+        //    }
+        //}
+
+
+        private double GetHeadshotRatio(int kills, int headshots)
+        {
+            if (kills > 0)
+            {
+                return Math.Round((double)headshots / (double)kills * 100.0, 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
