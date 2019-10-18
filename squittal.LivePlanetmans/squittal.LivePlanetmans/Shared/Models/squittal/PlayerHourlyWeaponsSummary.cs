d9426f1eb780f9f588a64861954a36e56e374ccc
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2019-10-18 13:05:33 -0500

    add player faction id to weapons summary model

--- squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/squittal/PlayerHourlyWeaponsSummary.cs
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/squittal/PlayerHourlyWeaponsSummary.cs
@@ -7,6 +7,7 @@ namespace squittal.LivePlanetmans.Shared.Models
     public class PlayerHourlyWeaponsSummary
     {
         public string PlayerId { get; set; }
+        public int PlayerFactionId { get; set; }
         public DateTime QueryStartTime { get; set; }
         public DateTime QueryNowUtc { get; set; }
 
