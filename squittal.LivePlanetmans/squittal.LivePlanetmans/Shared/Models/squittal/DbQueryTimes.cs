856cdf97b93ae95282be42f0cb645a7bb727201d
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2019-10-28 11:55:50 -0500

    add new models for sortable head-to-head view & db query times

---
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/squittal/DbQueryTimes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace squittal.LivePlanetmans.Shared.Models
+{
+    public class DbQueryTimes
+    {
+        public DateTime QueryStartTimeUtc { get; set; }
+        public DateTime QueryNowUtc { get; set; }
+
+        public TimeSpan QueryTimeSpan {
+            get
+            {
+                return QueryNowUtc - QueryStartTimeUtc;
+            }
+        }
+
+        public string RangeDisplayLocalized
+        {
+            get
+            {
+                return GetLocalizedDateRangeDisplay();
+            }
+        }
+
+
+        private string GetLocalizedDateRangeDisplay()
+        {
+            if (QueryStartTimeUtc == null || QueryNowUtc == null)
+            {
+                return string.Empty;
+            }
+
+            bool sameDates = (QueryStartTimeUtc.Date == QueryNowUtc.Date);
+            
+            var offsetHours = TimeSpan.FromHours(5);
+
+            return sameDates
+                    ? $"{GetOffsetShortTimeString(QueryStartTimeUtc, offsetHours)} - {GetOffsetShortTimeString(QueryNowUtc, offsetHours)}"
+                    : $"{GetOffsetLongTimeString(QueryStartTimeUtc, offsetHours)} - {GetOffsetLongTimeString(QueryNowUtc, offsetHours)}";
+        }
+
+        private string GetOffsetShortTimeString(DateTime time, TimeSpan offsetHours)
+        {
+            return (time - offsetHours).ToShortTimeString();
+        }
+
+        private string GetOffsetLongTimeString(DateTime time, TimeSpan offsetHours)
+        {
+            return $"{(time - offsetHours).ToString("M")} {(time - offsetHours).ToShortTimeString()}";
+        }
+    }
+}
---
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Shared/Models/squittal/HourlyPlayerHeadToHeadReport.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace squittal.LivePlanetmans.Shared.Models
+{
+    public class HourlyPlayerHeadToHeadReport
+    {
+        public PlayerDetails PlayerDetails { get; set; }
+        public DbQueryTimes QueryTimes { get; set; }
+
+        public IEnumerable<PlayerHeadToHeadSummaryRow> HeadToHeadSummaries { get; set; }
+
+        public PlayerHeadToHeadSummaryRow[] SortedHeadToHeadSummaries { get; set; }
+
+
+        private SortColumn _defaultSortColumn = SortColumn.Kills;
+        private SortDirection _defaultSortDirection = SortDirection.Descending;
+
+        private SortColumn _sortColumn;
+        private SortDirection _sortDirection;
+
+        public HourlyPlayerHeadToHeadReport()
+        {
+            _sortColumn = _defaultSortColumn;
+            _sortDirection = _defaultSortDirection;
+        }
+
+        public void SortReport(SortColumn column)
+        {
+            UpdateSortColumn(column);
+            UpdateSortDirection(column);
+
+            switch (column)
+            {
+                case SortColumn.Kills:
+                    HeadToHeadSummaries = SortByKills();
+                    break;
+
+                case SortColumn.Deaths:
+                    HeadToHeadSummaries = SortByDeaths();
+                    break;
+
+                case SortColumn.KillDeathRatio:
+                    HeadToHeadSummaries = SortByKillDeathRatio();
+                    break;
+
+                case SortColumn.Player:
+                    HeadToHeadSummaries = SortByPlayerName();
+                    break;
+
+                default:
+                    HeadToHeadSummaries = SortByKills();
+                    break;
+            }
+        }
+
+
+        private IEnumerable<PlayerHeadToHeadSummaryRow> SortByKills()
+        {
+            return (_sortDirection == SortDirection.Ascending)
+                ? HeadToHeadSummaries.OrderBy(h2h => h2h.PlayerStats.Kills) //.AsEnumerable()
+                : HeadToHeadSummaries.OrderByDescending(h2h => h2h.PlayerStats.Kills); //.AsEnumerable()
+        }
+
+        private IEnumerable<PlayerHeadToHeadSummaryRow> SortByDeaths()
+        {
+            return (_sortDirection == SortDirection.Ascending)
+                ? HeadToHeadSummaries.OrderBy(h2h => h2h.PlayerStats.Deaths)
+                : HeadToHeadSummaries.OrderByDescending(h2h => h2h.PlayerStats.Deaths);
+        }
+
+        private IEnumerable<PlayerHeadToHeadSummaryRow> SortByKillDeathRatio()
+        {
+            return (_sortDirection == SortDirection.Ascending)
+                ? HeadToHeadSummaries.OrderBy(h2h => h2h.PlayerStats.KillDeathRatio)
+                : HeadToHeadSummaries.OrderByDescending(h2h => h2h.PlayerStats.KillDeathRatio);
+        }
+
+        private IEnumerable<PlayerHeadToHeadSummaryRow> SortByPlayerName()
+        {
+            return (_sortDirection == SortDirection.Ascending)
+                ? HeadToHeadSummaries.OrderBy(h2h => h2h.EnemyDetails.PlayerName)
+                : HeadToHeadSummaries.OrderByDescending(h2h => h2h.EnemyDetails.PlayerName);
+        }
+
+
+        private SortColumn UpdateSortColumn(SortColumn column)
+        {
+            _sortColumn = column;
+            return _sortColumn;
+        }
+
+        private SortDirection UpdateSortDirection(SortColumn column)
+        {
+            _sortDirection = (column == _sortColumn)
+                                ? GetOppositeSortDirection(_sortDirection)
+                                : _defaultSortDirection;
+
+            return _sortDirection;
+        }
+
+        private SortDirection GetOppositeSortDirection(SortDirection direction)
+        {
+            return direction == SortDirection.Ascending
+                ? SortDirection.Descending
+                : SortDirection.Ascending;
+        }
+
+    }
+
+    public class PlayerHeadToHeadSummaryRow
+    {
+        public PlayerDetails EnemyDetails { get; set; }
+        
+        public DeathEventAggregate PlayerStats { get; set; }
+        public DeathEventAggregate EnemyStats { get; set; }
+    }
+
+    public class HeadToHeadSummaryRow
+    {
+        public PlayerDetails AttackerDetails { get; set; }
+        public PlayerDetails VictimDetails { get; set; }
+
+        public DeathEventAggregate AttackerStats { get; set; }
+        public DeathEventAggregate VictimStats { get; set; }
+    }
+
+    public enum SortDirection
+    {
+        Descending,
+        Ascending
+    }
+
+    public enum SortColumn
+    {
+        Kills,
+        Deaths,
+        KillDeathRatio,
+        Player
+    }
+}
