308432fa22a776c4c805d19a9c134da929f5f416
Chirtle <10554267+chirtle@users.noreply.github.com>
2019-12-06 17:21:07 -0600

    add batch file to start sql service & run app

---
+++ commands/RunApp.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo Starting SQL Server service...
+call :StartSqlService
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to start SQL Server service. Exiting...
+    pause
+    exit
+)
+
+echo Starting squittal.LivePlanetmans app...
+call :StartDotnetApp
+if %ERRORLEVEL% NEQ 0 (
+    echo Failed to start the app. Exiting...
+    pause
+    exit
+)
+pause
+
+
+:StartSqlService
+SC query MSSQL$SQLEXPRESS | FIND "STATE" | FIND "RUNNING" 2>&1 > nul
+if errorlevel 1 (net start MSSQL$SQLEXPRESS) else (echo The SQL Server service is already running... & echo.)
+exit /B
+
+:StartDotnetApp
+cd "%~dp0"
+dotnet run --verbosity m --project "..\squittal.LivePlanetmans\squittal.LivePlanetmans\Server\squittal.LivePlanetmans.Server.csproj"
+exit /B
