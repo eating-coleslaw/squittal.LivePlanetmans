6b072281a7b8a5a871a1393d06fd804d21057cbb
eating-coleslaw <10554267+eating-coleslaw@users.noreply.github.com>
2019-12-19 11:58:49 -0600

    remove commented-out code

--- squittal.LivePlanetmans/squittal.LivePlanetmans/Server/Services/Planetside/Interfaces/ICharacterService.cs
+++ squittal.LivePlanetmans/squittal.LivePlanetmans/Server/Services/Planetside/Interfaces/ICharacterService.cs
@@ -1,7 +1,4 @@
 ﻿using squittal.LivePlanetmans.Shared.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace squittal.LivePlanetmans.Server.Services.Planetside
